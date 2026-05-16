a24a233f98e4cb09d155617f1acb4a7dc32618a1
Vinxkumar <vinxkumar178@gmail.com>
2026-05-16 22:46:22 +0530

    Added Login route and loader routes to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import {Route, Routes} from 'react-router-dom'
+import {lazy, Suspense} from 'react'
+import PageLoader from './pages/PageLoader'
 
 import './App.css'
+
+const LoginPageLazy = lazy(() => import('./pages/LoginPage'))
 
 function App() {
   return (
     <>
 
-      
+<Routes>
+  <Route path='/' element={<PageLoader />} />
+  <Route path='/login' element={
+    
+    <Suspense fallback={
+      <div className="w-screen h-screen flex items-center justify-center bg-[#09090b]"/>
+    }>
+      <LoginPageLazy />
+    </Suspense>
+    } />
+
+</Routes>
 
 
     </>
