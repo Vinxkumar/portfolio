b838e53e74f1d7050745a5e96cb65637aaa72e95
Vinxkumar <vinxkumar178@gmail.com>
2026-05-16 22:44:40 +0530

    Boot Loader / Page Loader

---
+++ src/pages/PageLoader.tsx
@@ -0,0 +1,65 @@
+import DotField from "../components/DotField";
+import ShinyText from "../components/ShinyText";
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from "react";
+
+
+
+
+const preloadLogin = () => import('../pages/LoginPage')
+
+
+
+const PageLoader = () => {
+    const navigate = useNavigate()
+    //   const navigate = useNavigate()
+
+  useEffect(() => {
+    preloadLogin()
+
+    const t = setTimeout(() => navigate('/login'), 4000)
+    return () => clearTimeout(t)
+  }, [])
+  return (
+    <div className="w-screen h-screen flex  items-center justify-center bg-[#120F17]">
+      <div
+        style={{ width: "100%", height: "600px", position: "relative" }}
+        className="flex flex-col items-center justify-center"
+      >
+        <DotField
+          dotRadius={1}
+          dotSpacing={14}
+          bulgeStrength={43}
+          glowRadius={50}
+          sparkle={false}
+          waveAmplitude={0}
+          cursorRadius={150}
+          cursorForce={0.1}
+          bulgeOnly
+          gradientFrom="#A855F7"
+          gradientTo="#B497CF"
+          glowColor="#120F17"
+        />
+              <ShinyText
+        text="Folio OS"
+        speed={3.3}
+        delay={0}
+        color="#4a4949"
+        shineColor="#ffffff"
+        spread={120}
+        direction="left"
+        yoyo={false}
+        pauseOnHover={false}
+        disabled={false}
+        className="absolute text-4xl font-bold"
+      />
+      <span className="absolute text-3 font-light text-gray-600 mt-20">
+        A portfolio website .
+      </span>
+      {/* {timeout} */}
+      </div>
+    </div>
+  );
+};
+
+export default PageLoader;
