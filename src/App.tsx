6d8cf87e1044bc866330c6afd03cecf8f82f7331
Vinxkumar <vinxkumar178@gmail.com>
2026-05-17 18:23:15 +0530

    Added a  Home Page routung

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import {Route, Routes} from 'react-router-dom'
 import {lazy, Suspense} from 'react'
 import PageLoader from './pages/PageLoader'
-
 import './App.css'
 
 const LoginPageLazy = lazy(() => import('./pages/LoginPage'))
-
+const HomeScreenLazy = lazy(() => import("./pages/HomeScreen"))
 function App() {
   return (
     <>
@@ -20,6 +19,14 @@ function App() {
       <LoginPageLazy />
     </Suspense>
     } />
+
+    <Route path="/home" element={
+          <Suspense fallback={
+      <div className="w-screen h-screen flex items-center justify-center bg-[#09090b]"/>
+    }>
+      <HomeScreenLazy darkMode={true}/>
+    </Suspense>
+    }/>
 
 </Routes>
 
