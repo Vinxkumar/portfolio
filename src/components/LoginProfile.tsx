0d9252b5b5053e4f9dd2c124c6100ae05e469643
Vinxkumar <vinxkumar178@gmail.com>
2026-05-17 13:04:40 +0530

    Fiexd Bugs

--- src/components/LoginProfile.tsx
+++ src/components/LoginProfile.tsx
@@ -16,10 +16,10 @@ const LoginProfile = ({darkMode}: DarkModeProp) => {
         className = {`absolute p-4 justify-left items-center flex  gap-2 transition-all ease-in-out w-78 h-28 rounded-3xl shadow-lg ${
             darkMode ? "bg-[#18181b] border border-[#27272a] text-white" : "bg-[#f4f4f5] border border-[#c8c8c8]"
         }`}>
-            <button  
+            <div 
             className="absolute items-center h-20 w-20 rounded-full">
                 <img src={profilepic} alt={`${<CgProfile/>}`} className="h-full w-full  rounded-full"/>
-            </button>
+            </div>
             <div className="absolute ml-22 w-48 h-12  flex pl-3 flex-col items-start justify-left">
                 <h3 className="absolute text-2xl">Vinod Kumar S</h3>
                 <h5 className="absolute mt-8 text-xl text-gray-400">Software Engineer</h5>
