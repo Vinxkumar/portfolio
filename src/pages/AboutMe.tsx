3c3b2678d5fb340bdd97d06bd3591c3947e7e6c2
Vinxkumar <vinxkumar178@gmail.com>
2026-05-18 21:47:53 +0530

    Moved all the UI components to .ui/

---
+++ src/pages/AboutMe.tsx
@@ -0,0 +1,15 @@
+
+
+const AboutMe = () => {
+    return (
+        <>
+        <div className="relative z-10 bg-black w-screen h-screen">
+            <div className="absolute">
+                erere
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default AboutMe
