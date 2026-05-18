28f3b324dd60407c63cab1e953cd0e0c39a3b875
Vinxkumar <vinxkumar178@gmail.com>
2026-05-18 21:50:38 +0530

    Removed Unnecessary code and added landing page and aboutme section

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
 import {Route, Routes} from 'react-router-dom'
 import {lazy, Suspense} from 'react'
 import PageLoader from './pages/PageLoader'
+import { useState, useEffect } from 'react'
+import AboutMe from './pages/AboutMe'
 import './App.css'
 
-const LoginPageLazy = lazy(() => import('./pages/LoginPage'))
-const HomeScreenLazy = lazy(() => import("./pages/HomeScreen"))
+const HomeScreenLazy = lazy(() => import("./pages/LandingPage"))
+// const AboutMeScreenLazy = lazy(() => import())
 function App() {
+  const [scrollY, setScrollY] = useState(0);
+
+useEffect(() => {
+  const handleScroll = () => setScrollY(window.scrollY);
+  window.addEventListener("scroll", handleScroll);
+  return () => window.removeEventListener("scroll", handleScroll);
+}, []);
   return (
     <>
 
 <Routes>
   <Route path='/' element={<PageLoader />} />
-  <Route path='/login' element={
-    
-    <Suspense fallback={
-      <div className="w-screen h-screen flex items-center justify-center bg-[#09090b]"/>
-    }>
-      <LoginPageLazy />
-    </Suspense>
-    } />
-
     <Route path="/home" element={
           <Suspense fallback={
       <div className="w-screen h-screen flex items-center justify-center bg-[#09090b]"/>
     }>
-      <HomeScreenLazy darkMode={true}/>
+      <main>
+        <HomeScreenLazy scrollY={scrollY}/>
+        <AboutMe/>
+      </main>
     </Suspense>
     }/>
 
