07f150d4972b4790797554c435af7349a07aaa26
Vinxkumar <vinxkumar178@gmail.com>
2026-05-19 22:23:34 +0530

    Essential modifations

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { motion } from "motion/react";
+import { motion} from "motion/react";
 import ShapeGrid from "../components/ui/ShapeGrid";
 import { useState } from "react";
 import HomeNav from "../components/HomeNavBar";
@@ -6,20 +6,20 @@ import LandingPage from "../components/LandingContent"
 import MetaBalls from "../components/ui/MetalBalls";
 
 type ScrollPosition = {
-  scrollY: number;
+  scrolly: number;
 };
 
 // const preloadLogin = () => import("../pages/HomeScreen")
 
-const HomePage = ({scrollY}: ScrollPosition) => {
+const HomePage = ({scrolly} : ScrollPosition) => {
   const [darkMode, setDarkMode] = useState(true);
-
 
   return (
     <>
-      <div
-      className={`relative w-full min-h-screen flex pt-4  justify-between transition-colors duration-700  bg-[#09090b] overflow-hidden
-}`}
+      <motion.div
+
+      className={`relative w-full min-h-screen flex pt-4  justify-between transition-colors duration-700  bg-[#09090b] overflow-hidden 
+      `}
       >
         <div className="absolute z-10 w-full h-full">
           <ShapeGrid
@@ -34,10 +34,10 @@ const HomePage = ({scrollY}: ScrollPosition) => {
         </div>
 
         {/*  Home Secren Nav */}
-        <HomeNav scrollY={scrollY}/>
+        <HomeNav scrolly={scrolly}/>
 
         
-        <div className="absolute w-3/5 h-[70%] z-10 mt-25">
+        <div className="absolute w-3/5 h-[70%] z-10 mt-20">
           <LandingPage/>
         </div>
 
@@ -45,6 +45,7 @@ const HomePage = ({scrollY}: ScrollPosition) => {
   initial={{ opacity: 0, y: 40 }}
   animate={{ opacity: 1, y: 0 }}
   transition={{ delay: 1, duration: 1.25, ease: "anticipate" }}
+  
   className="absolute right-0 top-24 w-1/2 h-[70%] z-30 overflow-hidden"
 >
   <MetaBalls
@@ -66,11 +67,11 @@ const HomePage = ({scrollY}: ScrollPosition) => {
             animate={{ z: 10, opacity: 1 }}
             transition={{ delay: 2, duration: 0.5 }}
         
-        className="fixed left-1/2 bottom-24 z-10 text-xl transition-all ease-in-out duration-150 animate-bounce">
+        className="absolute left-1/2 bottom-24 z-10 text-xl transition-all ease-in-out duration-150 animate-bounce">
             <span className={`animate-pulse ${darkMode? " text-gray-400":"text-black"} h-0.5 w-1 items-center justify-center  rounded-full bg-white`}>.</span>
             
         </motion.div>
-      </div>
+      </motion.div>
     </>
   );
 };
