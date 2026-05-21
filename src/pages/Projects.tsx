4eba236da87f5026bc4dd8f8fb722b317905162a
Vinxkumar <vinxkumar178@gmail.com>
2026-05-21 18:21:22 +0530

    checking different color pallets

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,8 +5,12 @@ import BlurText from "../components/ui/BlurText"
 const Projects = () => {
     return (
         <>
-        <div id="projects" className="relative bg-[#09090b] pt-12  pb-30 text-white w-screen h-screen">
+        <div id="projects" className="relative z-10 bg-[#8EDB8F] pt-12 pb-30 text-white w-screen h-screen border-b border-white overflow-x-auto overflow-hidden">
+<span className=" ml-[95%] absolute z-15 top-4  right-0 text-transparent text-9xl [-webkit-text-stroke:1px_#8b5cf6]">
+  Projects
+</span>
                     <div className="absolute z-10 flex gap-2">
+                    
           <motion.div
             initial={{ z: -100, opacity: 0 }}
             whileInView={{ z: 0, opacity: 1 }}
@@ -31,15 +35,15 @@ const Projects = () => {
           transition={{
             delay: 0.8, duration:0.5
           }}
-          className="z-10 mt-10 absolute font-poppins text-4xl font-bold px-8 py-2">Projects</motion.div>
-                    <motion.div 
+          className="z-10 mt-10 absolute font-poppins text-[#0E2D2E] text-4xl font-bold px-8 py-2">Projects</motion.div>
+                    {/* <motion.div 
           initial={{y:100, opacity:0}}
           whileInView={{y:0, opacity:1}}
           viewport={{once: true}}
           transition={{
             delay: 0.5, duration:0.5
-          }}><ProjectCard /></motion.div>
-            
+          }}><ProjectCard /></motion.div> */}
+          {/* <div className="bottom-0 h-0.5 w-full bg-white"/> */}
         </div>
         </>
     )
