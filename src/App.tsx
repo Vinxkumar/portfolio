6c48f095e38a204b918d0fb131ae4fc020e8da90
Vinxkumar <vinxkumar178@gmail.com>
2026-05-22 16:06:35 +0530

    Updated About Me as second section

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,54 @@
-import {Route, Routes} from 'react-router-dom'
-import {lazy, Suspense} from 'react'
-import PageLoader from './pages/PageLoader'
-import { useState, useEffect } from 'react'
-import AboutMe from './pages/AboutMe'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import PageLoader from "./pages/PageLoader";
+import ClickSpark from "./components/ui/ClickSpark";
+import { useState, useEffect } from "react";
+import AboutMe from "./pages/AboutMe";
+import "./App.css";
 
-const HomeScreenLazy = lazy(() => import("./pages/LandingPage"))
-const ProjectsScreenLazy = lazy(() => import("./pages/Projects"))
-const Patents = lazy(() => import("./pages/Patents"))
-
+const HomeScreenLazy = lazy(() => import("./pages/LandingPage"));
+const ProjectsScreenLazy = lazy(() => import("./pages/Projects"));
+const Patents = lazy(() => import("./pages/Patents"));
 
 function App() {
-        const [scrolly, setScrollY] = useState(0);
-  
-    useEffect(() => {
-      const handleScroll = () => setScrollY(window.scrollY);
-      window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+  const [scrolly, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => setScrollY(window.scrollY);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <>
-
-<Routes>
-  <Route path='/' element={<PageLoader />} />
-    <Route path="/home" element={
-          <Suspense fallback={
-      <div className="w-screen h-screen flex items-center justify-center bg-[#09090b]"/>
-    }>
-      <main>
-
-    <HomeScreenLazy scrolly={scrolly}/>
-
-    <ProjectsScreenLazy />
-
-    <Patents/>
-    <AboutMe/>
-
-      </main>
-    </Suspense>
-    }/>
-
-</Routes>
+      <Routes>
+        <Route path="/" element={<PageLoader />} />
+        <Route
+          path="/home"
+          element={
+            <Suspense
+              fallback={
+                <div className="w-screen h-screen flex items-center justify-center bg-[#09090b]" />
+              }
+            >
+              <main>
+                {/* import ClickSpark from './ClickSpark'; */}
 
 
+  {/* Your content here */}
+
+                <HomeScreenLazy scrolly={scrolly} />
+                <AboutMe />
+                <ProjectsScreenLazy />
+
+                <Patents />
+                {/* </ClickSpark> */}
+              </main>
+            </Suspense>
+          }
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
