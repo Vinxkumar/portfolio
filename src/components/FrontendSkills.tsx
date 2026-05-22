6cf86d13105fb2809be7d4d606688fb2e7bcf95c
Vinxkumar <vinxkumar178@gmail.com>
2026-05-22 16:09:25 +0530

    Added skill section

---
+++ src/components/FrontendSkills.tsx
@@ -0,0 +1,34 @@
+
+
+type skillSize = 1 | 2 | 3
+
+type skill = {
+    name: string,
+    icon: string,
+    size: skillSize,
+    top: string,
+    left: string,
+    dur: number,
+    delay: number
+}
+
+const skills: skill[] = [
+    {
+        name : "React",
+        icon : 
+    }
+]
+
+const FrontendSkills = () => {
+
+    return (
+        <>
+            <div className="relative w-full h-full items-center justify-center">
+                
+            </div>
+        </>
+    )
+}
+
+export default FrontendSkills
+
