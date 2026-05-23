322db9148631cdae59e8ada36df45352e6f62867
Vinxkumar <vinxkumar178@gmail.com>
2026-05-23 23:22:22 +0530

    Added All the skills and its related properties for ui appearance

--- src/components/FrontendSkills.tsx
+++ src/components/FrontendSkills.tsx
@@ -4,19 +4,58 @@ type skillSize = 1 | 2 | 3
 
 type skill = {
     name: string,
-    icon: string,
+
     size: skillSize,
     top: string,
-    left: string,
+    right: string,
     dur: number,
     delay: number
 }
 
 const skills: skill[] = [
-    {
-        name : "React",
-        icon : 
-    }
+
+{ name: "React",  size: 3, top: "1%", right: "50%", dur: 2.1, delay: 0.8 },
+
+{ name: "TypeScript",   size: 3, top: "8%",  right: "70%", dur: 1.8, delay: 1.5 },
+
+{ name: "SpringBoot",size: 3, top: "0%",  right: "20%", dur: 2.4, delay: 0.3 },
+
+{ name: "tailwindCSS",size: 3, top: "18%", right: "58%", dur: 1.9, delay: 2.1 },
+
+{ name: "Vite",size: 3, top: "25%", right: "40%", dur: 2.2, delay: 1.0 },
+
+{ name: "GitHub",size: 3, top: "30%", right: "55%", dur: 1.7, delay: 2.6 },
+
+{ name: "Git",size: 3, top: "33%", right: "30%", dur: 2.5, delay: 0.5 },
+
+{ name: "AppScript",size: 3, top: "60%", right: "18%", dur: 2.0, delay: 3.1 },
+
+{ name: "PostgreSQL", size: 3, top: "10%", right: "10%", dur: 1.8, delay: 1.8 },
+
+{ name: "mongoDB", size: 3, top: "23%", right: "16%", dur: 2.3, delay: 0.7 },
+
+{ name: "flask", size: 3, top: "30%", right: "5%",  dur: 1.9, delay: 2.4 },
+
+{ name: "Java", size: 3, top: "45%", right: "60%", dur: 2.1, delay: 1.3 },
+
+{ name: "Python", size: 3, top: "40%", right: "12%", dur: 2.4, delay: 0.4 },
+
+{ name: "C/C++", size: 3, top: "60%", right: "65%", dur: 1.7, delay: 2.9 },
+
+{ name: "JavaScript", size: 3, top: "12%", right: "35%", dur: 2.0, delay: 0.2 },
+
+{ name: "HTML/5", size: 3, top: "55%", right: "45%", dur: 2.3, delay: 1.6 },
+
+{ name: "CSS/3", size: 3, top: "43%", right: "42%", dur: 1.8, delay: 3.2 },
+
+{ name: "Linux", size: 3, top: "75%", right: "12%", dur: 2.3, delay: 2.2 },
+
+{ name: "Research Work", size: 3, top: "85%", right: "50%", dur: 2.1, delay: 1.0 },
+
+{ name: "Software Engineer",  size: 3, top: "50%", right: "5%",  dur: 2.3, delay: 4.0 },
+
+{ name: "Full-Stack Developer", size: 3, top: "70%", right: "32%", dur: 2.2, delay: 2.0 },
+
 ]
 
 const FrontendSkills = () => {
