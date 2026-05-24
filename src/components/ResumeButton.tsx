e8a2b94e43fffbc73855bc5c1cd57b2f7318e0cd
Vinxkumar <vinxkumar178@gmail.com>
2026-05-24 21:35:30 +0530

    Changed the color pallet

--- src/components/ResumeButton.tsx
+++ src/components/ResumeButton.tsx
@@ -3,11 +3,11 @@ import { FiFileText } from "react-icons/fi";
 const ResumeButton = () => {
   return (
     <div
-      className="w-full h-full flex items-center justify-between bg-[#0E2D2E] px-4 cursor-pointer"
+      className="w-full h-full flex items-center justify-between bg-[#c05c40] px-4 cursor-pointer"
       onClick={() => window.open("https://www.linkedin.com/in/vinod-kumar-s-/")}
     >
-      <span className="font-poppins text-[#8edb8f]">Resume</span>
-      <FiFileText className="w-5 h-5 text-[#8edb8f]" />
+      <span className="font-poppins text-[#eee8d8]">Resume</span>
+      <FiFileText className="w-5 h-5 text-[#eee8d8]" />
     </div>
   );
 };
