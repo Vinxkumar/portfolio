2df808183d265d27164a7801e64ca097e6ab7d48
Vinxkumar <vinxkumar178@gmail.com>
2026-05-25 21:30:32 +0530

    Added projectProps

---
+++ src/types/project.ts
@@ -0,0 +1,12 @@
+export interface projectProp {
+    title: string,
+    description: string,
+    tech : string[],
+    img?: string,
+    link: string
+    status: "ongoing" | "complete"  
+
+    delay: number,
+    top:string,
+    left:string
+}
