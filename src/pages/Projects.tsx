b2fa3440ab97234ce5a23afe42149143850e82e9
Vinxkumar <vinxkumar178@gmail.com>
2026-05-25 21:30:13 +0530

    Changed the color theme

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,42 +7,24 @@ const Projects = () => {
 
     return (
         <>
-        <div id="projects" className="relative w-screen h-screen flex justify-center items-center bg-[#0E2D2E] text-[#8edb8f]">
+        <div id="projects" className="relative w-screen h-screen flex justify-center items-center bg-[#1e1e1e] text-[#eee8d8]">
 
-                <div
-          style={{ width: "100%", height: "100%", position: "relative" }}
-          className="absolute top-0 flex flex-col items-center justify-center"
-        >
-          <DotField
-            dotRadius={2}
-            dotSpacing={14}
-            bulgeStrength={43}
-            glowRadius={50}
-            sparkle={false}
-            waveAmplitude={0}
-            cursorRadius={150}
-            cursorForce={0.1}
-            bulgeOnly
-            gradientFrom="#A855F7"
-            gradientTo="#B497CF"
-            glowColor="transparent"
-          />
-        </div>
 
-        <div className="absolute justify-start items-start flex flex-col w-32 left-[1%] top-0 mt-[6%] text-[#0E2D2E]">
+
+        <div className="absolute justify-start items-start flex flex-col w-32 left-[1%] top-0 mt-[6%] text-[#eee8d8]">
           <BlurText
             text="Projects"
             delay={800}
             animateBy="words"
             direction="top"
-            className="font-poppins text-3xl font-bold text-[#8edb8f]"
+            className="font-poppins text-3xl font-bold text-[#e8805e]"
           />
           <motion.div
             initial={{ scaleX: 0, opacity: 1 }}
             whileInView={{ scaleX: 1, opacity: 1 }}
             viewport={{ once: true }}
             transition={{ delay: 0.5, duration: 0.9 }}
-            className="absolute -bottom-1 h-0.75 w-full origin-left rounded-full bg-[#8edb8f]"
+            className="absolute -bottom-1 h-0.75 w-full origin-left rounded-full bg-[#e8805e]"
           />
         </div >
 
