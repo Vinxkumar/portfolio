6cd2596397d1fc1e94a6cc4722a3a5312880fd75
Vinxkumar <vinxkumar178@gmail.com>
2026-05-26 14:48:13 +0530

    changed the color pallets

--- src/pages/Patents.tsx
+++ src/pages/Patents.tsx
@@ -1,33 +1,13 @@
 import { motion } from "framer-motion";
 import BlurText from "../components/ui/BlurText";
-import SmartMotionPreview from "../assets/SmartMotionPreview.png"
-import DotField from "../components/ui/DotField"
+import PatentsCards from "../components/PatentCards";
 const Patents = () => {
   return (
     <>
-      <div className="relative z-10  w-screen h-[80vh] bg-[#8edb8f] border-t border-[#2d1f5e] overflow-hidden ">
+      <div className="relative z-10  w-screen h-[80vh] bg-[#2c2c2c] overflow-hidden ">
          
-                <div
-          style={{ width: "100%", height: "100%", position: "relative" }}
-          className="absolute top-0 flex flex-col items-center justify-center"
-        >
-          <DotField
-            dotRadius={2}
-            dotSpacing={14}
-            bulgeStrength={43}
-            glowRadius={50}
-            sparkle={false}
-            waveAmplitude={0}
-            cursorRadius={150}
-            cursorForce={0.1}
-            bulgeOnly
-            gradientFrom="#A855F7"
-            gradientTo="#B497CF"
-            glowColor="transparent"
-          />
-        </div>
 
-        <div className="absolute justify-start items-start flex flex-col w-32 left-[2%] top-0 mt-[4%] text-[#0E2D2E]">
+        <div className="absolute justify-start items-start flex flex-col w-32 left-[2%] top-0 mt-[4%] text-[#e8805e]">
           <BlurText
             text="Patents"
             delay={800}
@@ -40,14 +20,16 @@ const Patents = () => {
             whileInView={{ scaleX: 1, opacity: 1 }}
             viewport={{ once: true }}
             transition={{ delay: 0.5, duration: 0.9 }}
-            className="absolute -bottom-1 h-0.75 w-full origin-left rounded-full bg-[#0E2D2E]"
+            className="absolute -bottom-1 h-0.75 w-full origin-left rounded-full bg-[#e8805e]"
           />
         </div >
 
+        <div className="absolute top-[18%] left-[2%] w-[95%] h-[76%] flex justify-center items-center ">
+          <PatentsCards/>
+        </div>
+
       </div>
 
-
-      {/* </div> */}
     </>
   );
 };
