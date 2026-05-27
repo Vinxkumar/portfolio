4da339dcfce09893823ee358e4e0173b7bf4fa77
Vinxkumar <vinxkumar178@gmail.com>
2026-05-27 20:54:43 +0530

    Updated project card placement to avoid look like a template

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,37 +2,34 @@ import ProjectCard from "../components/ProjectCards"
 import { motion } from "framer-motion"
 import BlurText from "../components/ui/BlurText"
 
-
 const Projects = () => {
-
     return (
-        <>
-        <div id="projects" className="relative w-screen h-screen flex justify-center items-center bg-[#1e1e1e] text-[#eee8d8]">
+        <div id="projects" className="w-screen min-h-screen flex flex-col bg-[#1e1e1e] text-[#eee8d8] px-[4%] py-[6%]">
+
+          <div className="relative flex flex-col w-fit mb-10">
+            <BlurText
+              text="Projects"
+              delay={800}
+              animateBy="words"
+              direction="top"
+              className="font-poppins text-3xl font-bold text-[#e8805e]"
+            />
+            <motion.div
+              initial={{ scaleX: 0, opacity: 1 }}
+              whileInView={{ scaleX: 1, opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.5, duration: 0.9 }}
+              className="absolute -bottom-1 h-0.5 w-full origin-left rounded-full bg-[#e8805e]"
+            />
+          </div>
 
 
 
-        <div className="absolute justify-start items-start flex flex-col w-32 left-[1%] top-0 mt-[6%] text-[#eee8d8]">
-          <BlurText
-            text="Projects"
-            delay={800}
-            animateBy="words"
-            direction="top"
-            className="font-poppins text-3xl font-bold text-[#e8805e]"
-          />
-          <motion.div
-            initial={{ scaleX: 0, opacity: 1 }}
-            whileInView={{ scaleX: 1, opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ delay: 0.5, duration: 0.9 }}
-            className="absolute -bottom-1 h-0.75 w-full origin-left rounded-full bg-[#e8805e]"
-          />
-        </div >
+<div className="w-full mt-[1%] h-[60vh] text-[#8edb8f]">
+  <ProjectCard/>
+</div>
 
-          <div className="absolute w-[95%] mt-[8%] h-[75%] text-[#8edb8f]">
-          <ProjectCard/>
         </div>
-        </div>
-        </>
     )
 }
 
