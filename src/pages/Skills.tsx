e1df002abf22651601c55cae712325e05ab190d5
Vinxkumar <vinxkumar178@gmail.com>
2026-05-28 23:39:58 +0530

    Added Skills component

---
+++ src/pages/Skills.tsx
@@ -0,0 +1,44 @@
+const Skills = () => {
+  return (
+    <div
+      id="skills"
+      className="w-screen h-screen pt-[4%] flex bg-[#1e1e1e] justify-between items-center text-white"
+    >
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="border w-1/3  h-[80%] border-white">
+          <div className="w-full h-1/2 items-center justify-center border">
+            rere
+          </div>
+          <div className="w-full h-1/2 border items-center justify-center">
+            rere
+          </div>
+        </div>
+        <div className="border flex flex-col justify-center items-center w-1/3 h-[80%] border-white">
+          <div className="w-full h-1/3 border">erewe</div>
+
+
+          <div className="w-full h-1/3  flex justify-between border">
+          
+            <div className="w-1/4 flex border items-center justify-center">eeee</div>
+            <div className="w-1/4 flex border items-center justify-center">eeee</div>
+            <div className="w-1/4 flex border items-center justify-center">eeee</div>
+            <div className="w-1/4 flex border items-center justify-center">eeee</div>
+          
+          </div>
+
+          <div className="w-full h-1/3 border">rerer</div>
+        </div>
+        <div className="border w-1/3 h-[80%] border-white">
+          <div className="w-full h-1/2 border items-center justify-center">
+            rere
+          </div>
+          <div className="w-full h-1/2 border items-center justify-center">
+            rere
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
