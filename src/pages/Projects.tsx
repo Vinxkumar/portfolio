76e5f5add48870bf469c5b3442fd7bf145929aab
Vinxkumar <vinxkumar178@gmail.com>
2026-06-06 23:19:23 +0530

    removed project heading and underline because current section is denoted in nav bar

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,24 +4,7 @@ import BlurText from "../components/ui/BlurText"
 
 const Projects = () => {
     return (
-        <div id="projects" className="w-screen min-h-screen flex flex-col bg-[#1e1e1e] text-[#eee8d8] px-[4%] py-[6%]">
-
-          <div className="relative flex flex-col w-fit mb-10">
-            <BlurText
-              text="Projects"
-              delay={800}
-              animateBy="words"
-              direction="top"
-              className="font-poppins text-3xl font-bold text-[#e8805e]"
-            />
-            <motion.div
-              initial={{ scaleX: 0, opacity: 1 }}
-              whileInView={{ scaleX: 1, opacity: 1 }}
-              viewport={{ once: true }}
-              transition={{ delay: 0.5, duration: 0.9 }}
-              className="absolute -bottom-1 h-0.5 w-full origin-left rounded-full bg-[#e8805e]"
-            />
-          </div>
+        <div id="projects" className="w-screen h-screen flex flex-col bg-gh-bg text-[#eee8d8] px-[4%] py-[6%]">
 
 
 
